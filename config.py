b618a258ca99770f0d564beffaeba0299655ac94
ragavan <78453005+Ruthless9890@users.noreply.github.com>
2024-07-03 18:19:44 -0700

    updated requirements

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # configuration variables such as linkedin credentials, search keywords, and states
 
-LINKEDIN_USERNAME = 'ragavan-ar'
+LINKEDIN_USERNAME = 'ragavan.ims@gmail.com'
 LINKEDIN_PASSWORD = 'Speed=MSDhoni07'
 STATES = ['Michigan', 'Illinois', 'California']
